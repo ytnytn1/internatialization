4c76758f786853a6e794a0148b5b85c395c6b90b
Ivan Grischenko <ytnytn1@gmail.com>
2021-03-12 12:13:47 +0700

    add prittier and tabs

---
+++ src/components/TabPanel.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+
+type TabPanelProps = {
+  value: number;
+  index: number;
+};
+
+export const TabPanel: React.FC<React.PropsWithChildren<TabPanelProps>> = ({
+  value,
+  index,
+  children,
+}) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,51 @@
+import { AppBar, makeStyles, Tab, Tabs } from '@material-ui/core';
+import React from 'react';
+import { TabPanel } from './TabPanel';
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export const SimpleTabs: React.FC = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
+          <Tab label="React-i18next" {...a11yProps(0)} />
+          <Tab label="LinguiJS" {...a11yProps(1)} />
+          <Tab label="React-intl" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </div>
+  );
+};
